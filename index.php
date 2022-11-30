f6038414c281ab1fb158bbb4ee80d373822109a0
neolabs-in <pard@Deepaks-MacBook-Air.local>
2022-11-30 15:03:04 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Pratt - Free Bootstrap 3 Theme">
+    <meta name="author" content="Alvarez.is - BlackTie.co">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title>Pratt - Free Bootstrap 3 Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+    
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/smoothscroll.js"></script>
+    
+
+  </head>
+
+  <body data-spy="scroll" data-offset="0" data-target="#navigation">
+
+    <!-- Fixed navbar -->
+	    <div id="navigation" class="navbar navbar-default navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="#"><b>NeoHives</b></a>
+	        </div>
+	        <div class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav">
+	            <li class="active"><a href="#home" class="smothscroll">Home</a></li>
+	            <li><a href="#desc" class="smothscroll">Description</a></li>
+	            <li><a href="#showcase" class="smothScroll">Showcase</a></li>
+	            <li><a href="#contact" class="smothScroll">Contact</a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </div>
+
+
+	<section id="home" name="home"></section>
+	<div id="headerwrap">
+	    <div class="container">
+	    	<div class="row centered">
+	     		<div class="col-lg-12">
+					<h1><b>NeoHives Organised deSchools</b></h1>
+                    <h2>Revolutionizing Schools; Reeconomizing Nations</h2>
+					<h3>Schools should focus on Learning; and nothing else at the cost of Learning.</h3>
+					<br>
+	    		</div>
+	    		
+	    		<div class="col-lg-2">
+	    			<h5></h5>
+	    			<p></p>
+	    			<!-- <img class="hidden-xs hidden-sm hidden-md" src="assets/img/arrow1.png"> -->
+	    		</div>
+	    		<div class="col-lg-8">
+	    			<img class="img-responsive" src="assets/img/org_purpose.png" alt="">
+	    		</div>
+	    		<div class="col-lg-2">
+	    			<br>
+	    			<!-- <img class="hidden-xs hidden-sm hidden-md" src="assets/img/arrow2.png"> -->`
+	    			<h5></h5>
+	    			<p></p>
+	    		</div>
+	    	</div>
+	    </div> <!--/ .container -->
+	</div><!--/ #headerwrap -->
+
+
+	<section id="desc" name="desc"></section>
+	<!-- INTRO WRAP -->
+	<div id="intro">
+		<div class="container">
+			<div class="row centered">
+				<h1>Educational Quality Assurance through Design of Learning Spaces;</h1>
+                <h1>scafolded with Futuristic Curriculums and Humanistic Nurturing</h1>
+                <h3>Life = LEARNING = Development => Facilitation + Empowerment + Formative Assessment</h3>
+				<br>
+				<br>
+				<div class="col-lg-4">
+					<img src="assets/img/intro01.png" alt="">
+					<h3>Community</h3>
+                    <h5>Humanity our Larger Self;</h5>
+                    <h5>Breath our Uniting Action;</h5>
+                    <h5>LEARNING our Birth Right!!!</h5>
+				</div>
+				<div class="col-lg-4">
+					<img src="assets/img/intro02.png" alt="">
+					<h3>Schedule</h3>
+                    <h5>Key Projects need to have fixed Deadlines; however,</h5>
+                    <h5>Learning is not Time Bound;the process happens at a</h5>
+                    <h5>Genuinely VARIABLE Pace for each Different Individual.</h5>
+				</div>
+				<div class="col-lg-4">
+					<img src="assets/img/intro03.png" alt="">
+					<h3>Monitoring</h3>
+                    <h5>All Learning involves DYNAMIC ORGANIC Interactions,</h5>
+                    <h5>to Facilitate and Empower by FORMATIVE Assessment;</h5>
+                    <h5>if PURPOSEFUL, is it Time for Summative Examination?</h5>
+				</div>
+			</div>
+			<br>
+			<hr>
+	    </div> <!--/ .container -->
+	</div><!--/ #introwrap -->
+	
+	<!-- FEATURES WRAP -->
+	<div id="features">
+		<div class="container">
+			<div class="row">
+				<h1 class="centered">Why to How to What we do? Who we are? When and Where?</h1>
+				<br>
+				<br>
+				<div class="col-lg-6 centered">
+					<img class="centered" src="assets/img/tribute_to_a_teacher.jpg" alt="">
+				</div>
+                
+                <div class="col-lg-6">
+					<h3>Philanthropic Initiative</h3>
+					<br>
+				<!-- ACCORDION -->
+		            <div class="accordion ac" id="accordion2">
+		                <div class="accordion-group">
+		                    <div class="accordion-heading">
+		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+		                        Mission and Vision
+		                        </a>
+		                    </div><!-- /accordion-heading -->
+		                    <div id="collapseOne" class="accordion-body collapse in">
+		                        <div class="accordion-inner">
+                                    <h3>Neo Schools: Kids' Success Pools</h3>
+                                    <h5>Why?</h5>			
+                                    <p>Our world today ... God left this place long ago!
+... Society based on fear and greed of the ego;
+New generations can progress by leaps & bounds
+by mastering their world and taming its hounds.</p>
+                                    <h5>How?</h5>			
+                                    <p>Let's educate kids for creative entrepreneurship
+to establish local economies, in their leadership!
+Today we have schools wasting students' lives;
+Let's convert these institutions into Neo hives.</p>
+                                    <h5>What?</h5>			
+                                    <p>Kids build Neo labs, learning green technology;
+Experts act as Project Managers cum Teachers!
+Kids grow in physique, attitude & spirituality,
+without divisive religions and their Preachers!
+</p>
+								</div><!-- /accordion-inner -->
+		                    </div><!-- /collapse -->
+		                </div><!-- /accordion-group -->
+		                <br>
+		
+		                <div class="accordion-group">
+		                    <div class="accordion-heading">
+		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
+		                        Our People
+		                        </a>
+		                    </div>
+		                    <div id="collapseTwo" class="accordion-body collapse">
+		                        <div class="accordion-inner">
+								<p>Tushar Tamhane = Educational Consultant, Trustee</p>
+                                <p>Deepak Paranjape = Founder, Director of Curriculum, Design and Technology</p>
+                                <p>Durgesh Paliwal = Corporate Lawyer, Company Secretary</p>
+                                <p>Tejas Varavdekar = Digital Technology Manager</p>
+                                <p>Rajendra Singh = Material Procurement Executive</p>
+								</div><!-- /accordion-inner -->
+		                    </div><!-- /collapse -->
+		                </div><!-- /accordion-group -->
+		                <br>
+		
+		                 <div class="accordion-group">
+		                    <div class="accordion-heading">
+		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
+		                        Partners
+		                        </a>
+		                    </div>
+		                    <div id="collapseThree" class="accordion-body collapse">
+		                        <div class="accordion-inner">
+                                    <p>Technology Partners: Meetings in progress; None confirmed yet</p>
+                                    <p>Curriculum Partners: Preliminary negotiations successful; Hindi for elementary & English for secondary: Issues resolved for Project No.1</p>
+                                    <p>Funding Partners: Communication > Meetings   - “miniscule” positive commitments till date!</p>
+<p>Local School Leader Partners: Meetings lined up for shortlisted locations but not prioritized!
+</p>
+								</div><!-- /accordion-inner -->
+		                    </div><!-- /collapse -->
+		                </div><!-- /accordion-group -->
+		                <br>
+		                
+		                 <div class="accordion-group">
+		                    <div class="accordion-heading">
+		                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseFour">
+		                        Projects
+		                        </a>
+		                    </div>
+		                    <div id="collapseFour" class="accordion-body collapse">
+		                        <div class="accordion-inner">
+                                    <p>Project No.1 to be begun in August 2015; this First Neo School, with student quorum filled by admissions, will be ready to be handed over to School Management Partner by June 2016.</p>
+                                    <p>4 locations under consideration for Project No.1; Towns near the locations: Bengaluru, Indore, Delhi or Satara</p> 
+                                    <p>Parameters involved in Identifying Strategic Location:</p>
+                                    <p>Availability of State-of-the-Art Curriculum in Local Language > flowing into a Global one!<p>
+                                    <p>Strong “Enlightened” Local School Leaders?</p>
+<p>Availability of ‘Teaching/Resources’ to guide State-of-the-Art Technology Learning projects</p>
+<p>Open minded Parents for Student Quorum!!</p>
+<p>‘Visible’ Impact for Growth of Neo venture!?</p>
+
+								</div><!-- /accordion-inner -->
+		                    </div><!-- /collapse -->
+		                </div><!-- /accordion-group -->
+		                <br>			
+					</div><!-- Accordion -->
+				</div>
+			</div>
+		</div><!--/ .container -->
+	</div><!--/ #features -->
+	
+	
+	<section id="showcase" name="showcase"></section>
+	<div id="showcase">
+		<div class="container">
+			<div class="row">
+				<h1 class="centered">Rationalizing the Concept of our Initiative</h1>
+				<br>
+				<div class="col-lg-8 col-lg-offset-2">
+					<div id="carousel-example-generic" class="carousel slide">
+					  <!-- Indicators -->
+					  <ol class="carousel-indicators">
+					    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="100"></li>
+					  </ol>
+					
+					  <!-- Wrapper for slides --> 
+					  <div class="carousel-inner">
+					    <div class="item active">
+					      <img src="assets/img/teacher_role.png" alt="">
+					    </div>
+                        <div class="item">
+					      <img src="assets/img/education_for_what.png" alt="">
+					    </div>
+                        <div class="item">
+					      <img src="assets/img/newstory1.png" alt="">
+					    </div>
+                        <div class="item">
+					      <img src="assets/img/newstory2.png" alt="">
+					    </div>
+                        <div class="item">
+					      <img src="assets/img/neoliberator.jpg" alt="">
+					    </div>
+                        <div class="item">
+					      <img src="assets/img/neokids.png" alt="">
+					    </div>
+                        <div class="item">
+					      <img src="assets/img/apple_recruitment.jpg" alt="">
+					    </div>
+                        <div class="item">
+					      <img src="assets/img/global_challenges.png" alt="">
+					    </div>
+                        
+					  </div>
+					</div>
+				</div>
+			</div>
+			<br>
+			<br>
+			<br>	
+		</div><!-- /container -->
+	</div>	
+
+
+	<section id="contact" name="contact"></section>
+	<div id="footerwrap">
+		<div class="container">
+			<div class="col-lg-5">
+				<h3>Address</h3>
+				<p>
+				F-9/0:4, Shantivan CHSL,<br/>
+				Sector 7, Sanpada,<br/>
+				Navi Mumbai - 400705<br/>
+				Maharashtra, India
+				</p>
+			</div>
+			
+			<div class="col-lg-7">
+				<h3>Drop Us A Line (Sorry, not now!)</h3>
+                <h3>[database under construction till 21/6/2015]</h3>
+				<br>
+				<form role="form" action="#" method="post" enctype="plain"> 
+				  <div class="form-group">
+				    <label for="name1">Your Name</label>
+				    <input type="name" name="Name" class="form-control" id="name1" placeholder="Your Name">
+				  </div>
+				  <div class="form-group">
+				    <label for="email1">Email address</label>
+				    <input type="email" name="Mail" class="form-control" id="email1" placeholder="Enter email">
+				  </div>
+				  <div class="form-group">
+				  	<label>Your Text</label>
+				  	<textarea class="form-control" name="Message" rows="3"></textarea>
+				  </div>
+				  <br>
+				  <button type="submit" class="btn btn-large btn-success">SUBMIT</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div id="c">
+		<div class="container">
+			<p>Created using a template from <a href="http://www.blacktie.co">BLACKTIE.CO</a></p>
+		
+		</div>
+	</div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/bootstrap.js"></script>
+	<script>
+	$('.carousel').carousel({
+	  interval: 3500
+	})
+	</script>
+  </body>
+</html>
